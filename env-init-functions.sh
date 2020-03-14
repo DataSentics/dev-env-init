7e389e7fee05ba71c7cac81cf3dee62a352b6f5d
Jiri Koutny <jirkakoutny@gmail.com>
2020-03-14 15:56:52 +0000

    .bat conda scripts not necessary anymore
    .sh conda scripts works in Git bash on Windows as well

--- env-init-functions.sh
+++ env-init-functions.sh
@@ -153,14 +153,12 @@ set_conda_scripts() {
   echo "Seting-up conda/activate.d"
   local CONDA_ACTIVATE_DIR="$CONDA_ENV_PATH/etc/conda/activate.d"
   mkdir -p $CONDA_ACTIVATE_DIR
-  curl "https://raw.githubusercontent.com/DataSentics/dev-env-init/master/windows/conda/activate.d/env_vars.bat?$(date +%s)" --silent > "$CONDA_ACTIVATE_DIR/env_vars.bat"
   curl "https://raw.githubusercontent.com/DataSentics/dev-env-init/master/unix/conda/activate.d/env_vars.sh?$(date +%s)" --silent > "$CONDA_ACTIVATE_DIR/env_vars.sh"
   chmod +x "$CONDA_ACTIVATE_DIR/env_vars.sh"
 
   echo "Seting-up conda/deactivate.d"
   local CONDA_DEACTIVATE_DIR="$CONDA_ENV_PATH/etc/conda/deactivate.d"
   mkdir -p $CONDA_DEACTIVATE_DIR
-  curl "https://raw.githubusercontent.com/DataSentics/dev-env-init/master/windows/conda/deactivate.d/env_vars.bat?$(date +%s)" --silent > "$CONDA_DEACTIVATE_DIR/env_vars.bat"
   curl "https://raw.githubusercontent.com/DataSentics/dev-env-init/master/unix/conda/deactivate.d/env_vars.sh?$(date +%s)" --silent > "$CONDA_DEACTIVATE_DIR/env_vars.sh"
   chmod +x "$CONDA_DEACTIVATE_DIR/env_vars.sh"
 }
