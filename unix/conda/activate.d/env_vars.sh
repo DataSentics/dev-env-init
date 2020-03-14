f13cf364f55f40bd0ed75a7a3abb3901e4b42890
Jiri Koutny <jirkakoutny@gmail.com>
2020-03-14 15:51:32 +0000

    Env vars can be defined using .env file
    Variables are set as soon as the Conda environment is activated

--- unix/conda/activate.d/env_vars.sh
+++ unix/conda/activate.d/env_vars.sh
@@ -2,7 +2,12 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT_ROOT="$(sed "s|/\.venv/etc/conda/activate\.d$||" <<< $CURRENT_DIR)"
 
+export PROJECT_ROOT
 export PYTHONDONTWRITEBYTECODE=1
 export PYTHONPATH="$PROJECT_ROOT/src"
+
+if [ -f "$PROJECT_ROOT/.env" ]; then
+  set -o allexport; source "$PROJECT_ROOT/.env"; set +o allexport
+fi
 
 cd "$PROJECT_ROOT" || exit
