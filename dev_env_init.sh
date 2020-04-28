c5f2ef495a359b943e5275e5acc958834aba85e7
Jiri Koutny <jirkakoutny@gmail.com>
2020-04-28 08:08:20 +0200

    Databricks specific functions separated into https://github.com/bricksflow/dev-env-init

--- dev_env_init.sh
+++ dev_env_init.sh
@@ -255,15 +255,6 @@ create_git_hooks() {
   fi
 }
 
-download_winutils_on_windows() {
-  if [ $IS_WINDOWS == 1 ]; then
-    echo "Downloading Hadoop winutils.exe"
-
-    mkdir -p "$CONDA_ENV_PATH/hadoop/bin"
-    curl https://raw.githubusercontent.com/steveloughran/winutils/master/hadoop-3.0.0/bin/winutils.exe --silent > "$CONDA_ENV_PATH/hadoop/bin/winutils.exe"
-  fi
-}
-
 set_conda_scripts() {
   echo "Setting up Conda activation & deactivation scripts"
 
@@ -278,53 +269,6 @@ set_conda_scripts() {
   mkdir -p $CONDA_DEACTIVATE_DIR
   curl "https://raw.githubusercontent.com/DataSentics/dev-env-init/$ENV_INIT_BRANCH/unix/conda/deactivate.d/env_vars.sh?$(date +%s)" --silent > "$CONDA_DEACTIVATE_DIR/env_vars.sh"
   chmod +x "$CONDA_DEACTIVATE_DIR/env_vars.sh"
-}
-
-download_java() {
-  local JAVA_DIR="$HOME/.databricks-connect-java"
-
-  if [ -d "$JAVA_DIR" ]; then
-    echo "$JAVA_DIR already exists"
-    return
-  fi
-
-  echo "Downloading Java 1.8 to $JAVA_DIR"
-
-  mkdir -p $JAVA_DIR
-
-  local JAVA_ZIP_DIR="$JAVA_DIR/jdk8u242-b08"
-
-  if [ $IS_WINDOWS == 1 ]; then
-    curl https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u242-b08/OpenJDK8U-jdk_x64_windows_hotspot_8u242b08.zip -L --silent > "$JAVA_DIR/java.zip"
-    unzip -qq "$JAVA_DIR/java.zip" -d "$JAVA_DIR"
-    mv "$JAVA_ZIP_DIR/"* $JAVA_DIR
-    rm -rf "$JAVA_ZIP_DIR"
-    rm -rf "$JAVA_DIR/java.zip"
-    rm -rf "$JAVA_DIR/src.zip"
-  elif [ $DETECTED_OS == "mac" ]; then
-    curl https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u242-b08/OpenJDK8U-jdk_x64_mac_hotspot_8u242b08.tar.gz -L --silent > "$JAVA_DIR/java.tar.gz"
-    tar -xzf "$JAVA_DIR/java.tar.gz" -C "$JAVA_DIR"
-    mv "$JAVA_ZIP_DIR/Contents/Home/"* $JAVA_DIR
-    rm -rf "$JAVA_ZIP_DIR"
-    rm -rf "$JAVA_DIR/java.tar.gz"
-    chmod +x "$CONDA_ENV_PATH/lib/python3.7/site-packages/pyspark/bin/"*
-  elif [ $DETECTED_OS == "linux" ]; then
-    curl https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u242-b08/OpenJDK8U-jdk_x64_linux_hotspot_8u242b08.tar.gz -L --silent > "$JAVA_DIR/java.tar.gz"
-    tar -xzf "$JAVA_DIR/java.tar.gz" -C "$JAVA_DIR"
-    mv "$JAVA_ZIP_DIR/"* $JAVA_DIR
-    rm -rf "$JAVA_ZIP_DIR"
-    rm -rf "$JAVA_DIR/java.tar.gz"
-    chmod +x "$CONDA_ENV_PATH/lib/python3.7/site-packages/pyspark/bin/"*
-  fi
-}
-
-create_databricks_connect_config() {
-  # .databricks-connect file must always exist and contain at least empty JSON for the Databricks Connect to work properly
-  # specific cluster connection credentials must be set when creating the SparkSession instance
-  if [ ! -f "$HOME/.databricks-connect" ]; then
-    echo "Creating empty .databricks-connect file"
-    echo "{}" > "$HOME/.databricks-connect"
-  fi
 }
 
 create_dot_env_file() {
@@ -364,28 +308,9 @@ base_environment_setup() {
   set_conda_scripts
 }
 
-databricks_environment_setup() {
-  download_winutils_on_windows
-  download_java
-  create_databricks_connect_config
-}
-
 # main invocation functions ---------------------
-
-prepare_environment_databricks_app() {
-  base_environment_setup
-  databricks_environment_setup
-  create_dot_env_file
-  show_installation_finished_info
-}
 
 prepare_environment_for_package() {
   base_environment_setup
-  show_installation_finished_info
-}
-
-prepare_environment_for_package_with_databricks() {
-  base_environment_setup
-  databricks_environment_setup
   show_installation_finished_info
 }
